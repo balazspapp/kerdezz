670eda16c723868327cd5f90c74c1f8f5352b8e5
Papp Balázs <papp.balazs@sonrisa.hu>
2023-04-29 19:44:51 +0200

    add discovery-server

---
+++ discovery-server/src/test/kotlin/hu/gde/kerdezz/discoveryserver/DiscoveryServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.gde.kerdezz.discoveryserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
