bcb628d91ed835070680cbe813df0773c553f1e4
Papp Balázs <papp.balazs@sonrisa.hu>
2023-05-16 04:04:48 +0200

    realm-export.json and minor config changes. Move html template to component.

--- kerdezz-frontend/src/app/answer-options/answer-options.component.ts
+++ kerdezz-frontend/src/app/answer-options/answer-options.component.ts
@@ -3,7 +3,24 @@ import {Option} from "../domain/survey";
 
 @Component({
   selector: 'app-answer-options',
-  templateUrl: './answer-options.component.html',
+  template:`<div class="form-group">
+    <div class="row py-1" *ngFor="let item of options; let i = index">
+      <div class="col-11">
+        <input type="text" class="form-control" [(ngModel)]="options[i].value"/>
+      </div>
+      <div class="col-1">
+        <button class="btn btn-close" (click)="removeOption(i)"></button>
+      </div>
+    </div>
+    <div class="row py-1">
+      <div class="col-3">
+        <button class="btn btn-light rounded-0" (click)="addOption()">
+          <i class="bi bi-arrow-return-right" (click)="addOption()"></i>
+          Új lehetőség
+        </button>
+      </div>
+    </div>
+  </div>`,
   styleUrls: ['./answer-options.component.css']
 })
 export class AnswerOptionsComponent {
